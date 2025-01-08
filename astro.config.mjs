329af6050191c6a017b9dabff073f0543f07b2ff
Pascal9000 <125824455+Pascal9000@users.noreply.github.com>
2025-01-08 12:58:21 +0000

    test new base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://Pascal9000/github.io",
-  base: "/navbar-test",
+  base: "/navbar-test/",
 });
