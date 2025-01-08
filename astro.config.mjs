399ac3d62f652152efbe3ff6f5defecb56ea3a33
Pascal9000 <125824455+Pascal9000@users.noreply.github.com>
2025-01-08 15:51:56 +0000

    added NavBar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://Pascal9000/github.io",
   base: "/navbar-test/",
+  integrations: [tailwind()],
 });
