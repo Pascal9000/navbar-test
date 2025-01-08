a7197d18ed6ee7d86f97777006b87e0734240dbd
Pascal9000 <125824455+Pascal9000@users.noreply.github.com>
2025-01-08 12:43:44 +0000

    added base repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Pascal9000/github.io"
+  site: "https://Pascal9000/github.io",
+  base: "/navbar-test",
 });
