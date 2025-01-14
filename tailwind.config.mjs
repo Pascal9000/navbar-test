114656111947da64b0830bc8a1f67eb9ca7e0b6c
Pascal9000 <125824455+Pascal9000@users.noreply.github.com>
2025-01-14 13:07:16 +0000

    feat(card): add new card component with responsive design
    Implemented a new card component featuring:
    - An image with aspect ratio handling and responsive widths.
    - Title and description sections styled with custom fonts.
    - Hidden category and date metadata, designed for desktop view.
    
    This component includes configurable background color support via props (`bgColor`) and provides flexibility for various use cases.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#F0EEE6",
+				secondary: "#FAF9F5",
+			}
+		},
 	},
 	plugins: [],
 }
